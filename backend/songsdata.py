014b52b767e6a5256c8160d047adf2ac2d0b77fc
Ag <awesomeag61@gmail.com>
2022-02-06 12:33:48 -0800

    api finished
    finished api with AI

---
+++ backend/songsdata.py
@@ -0,0 +1,163 @@
+global happy_songs
+global sads_ongs
+global angry_songs
+
+happy_songs = [
+    {
+    "danceability": 0.761,
+    "energy": 0.525,
+    "key": 11,
+    "loudness": -6.9,
+    "mode": 1,
+    "speechiness": 0.0944,
+    "acousticness": 0.44,
+    "instrumentalness": 6.7e-06,
+    "liveness": 0.0921,
+    "valence": 0.531,
+    "tempo": 80.87
+  },
+  {
+  "danceability": 0.664,
+  "energy": 0.609,
+  "key": 1,
+  "loudness": -6.509,
+  "mode": 1,
+  "speechiness": 0.0707,
+  "acousticness": 0.304,
+  "instrumentalness": 0,
+  "liveness": 0.0926,
+  "valence": 0.194,
+  "tempo": 130.041
+},
+  {
+    "danceability": 0.741,
+    "energy": 0.691,
+    "key": 10,
+    "loudness": -7.395,
+    "mode": 0,
+    "speechiness": 0.0672,
+    "acousticness": 0.0221,
+    "instrumentalness": 0,
+    "liveness": 0.0476,
+    "valence": 0.892,
+    "tempo": 150.087,
+  },
+    {
+    "danceability": 0.824,
+    "energy": 0.764,
+    "key": 5,
+    "loudness": -4.175,
+    "mode": 0,
+    "speechiness": 0.0854,
+    "acousticness": 0.0888,
+    "instrumentalness": 0.00294,
+    "liveness": 0.117,
+    "valence": 0.881,
+    "tempo": 107.998,
+  }
+]
+
+angry_songs = [
+  {
+    "danceability": 0.695,
+    "energy": 0.54,
+    "key": 4,
+    "loudness": -5.692,
+    "mode": 1,
+    "speechiness": 0.0493,
+    "acousticness": 0.299,
+    "instrumentalness": 0,
+    "liveness": 0.367,
+    "valence": 0.415,
+    "tempo": 121.932,
+  },
+    {
+    "danceability": 0.563,
+    "energy": 0.664,
+    "key": 9,
+    "loudness": -5.044,
+    "mode": 1,
+    "speechiness": 0.154,
+    "acousticness": 0.335,
+    "instrumentalness": 0,
+    "liveness": 0.0849,
+    "valence": 0.688,
+    "tempo": 166.928,
+  },
+    {
+    "danceability": 0.535,
+    "energy": 0.388,
+    "key": 7,
+    "loudness": -9.805,
+    "mode": 0,
+    "speechiness": 0.155,
+    "acousticness": 0.507,
+    "instrumentalness": 0,
+    "liveness": 0.11,
+    "valence": 0.412,
+    "tempo": 106.703,
+  }
+]
+
+sad_songs = [
+  {
+    "danceability": 0.604,
+    "energy": 0.366,
+    "key": 5,
+    "loudness": -7.519,
+    "mode": 1,
+    "speechiness": 0.0282,
+    "acousticness": 0.578,
+    "instrumentalness": 0,
+    "liveness": 0.133,
+    "valence": 0.13,
+    "tempo": 141.981,
+  },
+    {
+    "danceability": 0.575,
+    "energy": 0.648,
+    "key": 10,
+    "loudness": -4.891,
+    "mode": 1,
+    "speechiness": 0.0358,
+    "acousticness": 0.583,
+    "instrumentalness": 0,
+    "liveness": 0.115,
+    "valence": 0.466,
+    "tempo": 75.977,
+  },
+    {
+    "danceability": 0.735,
+    "energy": 0.795,
+    "key": 11,
+    "loudness": -6.523,
+    "mode": 0,
+    "speechiness": 0.113,
+    "acousticness": 0.0296,
+    "instrumentalness": 3.18e-05,
+    "liveness": 0.0678,
+    "valence": 0.905,
+    "tempo": 122.0,
+  }
+]
+
+test_dict = {
+    "danceability": 1.0,
+    "energy": 0.524,
+    "key": 11,
+    "loudness": -6.9,
+    "mode": 1,
+    "speechiness": 0.542,
+    "acousticness": 0.341,
+    "instrumentalness": 6.7e-06,
+    "liveness": 0.1243,
+    "valence": 0.412,
+    "tempo": 93.65,
+    "type": "audio_features",
+    "id": "3USxtqRwSYz57Ewm6wWRMp",
+    "uri": "spotify:track:3USxtqRwSYz57Ewm6wWRMp",
+    "track_href": "https://api.spotify.com/v1/tracks/3USxtqRwSYz57Ewm6wWRMp",
+    "analysis_url": "https://api.spotify.com/v1/audio-analysis/3USxtqRwSYz57Ewm6wWRMp",
+    "duration_ms": 238805,
+    "time_signature": 4
+  }
--- backend/spotifyhook.py
+++ backend/spotifyhook.py
@@ -2,29 +2,106 @@ import spotipy
 import os
 from spotipy.oauth2 import SpotifyClientCredentials
 import sys
+import songsdata
+from songsdata import happy_songs, sad_songs, angry_songs, test_dict
+from sklearn import tree
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
 
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id="", client_secret=""))
-# example: 
-#results = spotify.search(q='genre:pop', limit=10, type="track", market=["US"])
-#print(results)
+
+## AI Model Training
+
+training_dicts = happy_songs + sad_songs + angry_songs
+training_labels = ["happy"] * len(happy_songs) + ["sad"] * len(sad_songs) + ["angry"] * len(angry_songs)
+vectorizer = DictVectorizer()
+vectorizer.fit(training_dicts)
+training_vectors = vectorizer.transform(training_dicts)
+testing_vectors = vectorizer.transform(test_dict)
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(training_vectors, training_labels)
+print(classifier.predict(testing_vectors))
+tree.export_graphviz(
+    classifier,
+    out_file='tree.dot',
+    feature_names=vectorizer.get_feature_names(),
+) 
+
+def manual_classify(song):
+    song = song[0]
+    if song["tempo"] < 90 and song["speechiness"] < 0.04:
+        return "sad"
+    if song["tempo"] > 110 or song["speechiness"] > 0.4 :
+        return "angry"
+    else:
+        return "happy"
+
+
+##
+
+
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id="17037de2bf4f479b817cc1d40ef2d4c2", client_secret="1b8adca14e0741dfba733d83754ab015"))
+
+def get_uris(data):
+    items = data["tracks"]["items"]
+    uris = []
+    for item in items:
+        uris.append(item["uri"])
+    return uris
+
+
+def get_mood(uri):
+    return spotify.audio_features(uri)
+    
+
+def infotomood(item):
+    energy=item[0]["energy"]
+    loudness=item[0]["loudness"]
+    speech=item[0]["speechiness"]
+    live=item[0]["liveness"]
+    tempo=item[0]["tempo"]
+    predictions = []
+    prediction = manual_classify(item)
+    predictions.append(prediction)
+    return predictions[0]
 
 
 def getsongs(mood, genre, limit, offset=0):
-    print(genre)
-    genremusic = spotify.search(q="genre:"+genre, limit=limit, type="track", market="US")
-    length = len(genremusic["tracks"]["items"])
-
     genre_dict = {"songs":[]}
-    for i in range(0, length):
-        tempdict = {}
-        tempdict["name"] = genremusic["tracks"]["items"][i]["name"]
-        tempdict["preview"] = genremusic["tracks"]["items"][i]["preview_url"]
-        tempdict["artist"] =  genremusic["tracks"]["items"][i]["artists"][0]["name"]
-        tempdict["uri"] = genremusic["tracks"]["items"][i]["uri"]
-        tempdict["id"] = genremusic["tracks"]["items"][i]["id"]
-        genre_dict["songs"].append(tempdict)
-
-    return genre_dict
+    off = 0
+    while len(genre_dict["songs"]) < 21:
+        genremusic = spotify.search(q="genre:"+genre, limit=50, offset=50, type="track", market="US")
+        length = len(genremusic["tracks"]["items"])
+        for i in range(0, length):
+            if infotomood(get_mood(genremusic["tracks"]["items"][i]["uri"])) == mood:
+                print('match found')
+                tempdict = {}
+                tempdict["name"] = genremusic["tracks"]["items"][i]["name"]
+                tempdict["preview"] = genremusic["tracks"]["items"][i]["preview_url"]
+                tempdict["artist"] =  genremusic["tracks"]["items"][i]["artists"][0]["name"]
+                tempdict["uri"] = genremusic["tracks"]["items"][i]["uri"]
+                tempdict["id"] = genremusic["tracks"]["items"][i]["id"]
+                genre_dict["songs"].append(tempdict)
+            else:
+                print(f"Got " + infotomood(get_mood(genremusic["tracks"]["items"][i]["uri"])) +" expected " + mood)
+        off += 50
     
 
+        
+    return genre_dict
+
+
+
+
+
+
+test = '''
+
+results = spotify.search(q="genre:pop", limit=10, type="track", market=["US"])
+data = results["tracks"]["items"]
+for item in data:
+    item_uri = item["uri"]
+    info = get_mood(item_uri)
+    mood = infotomood(info)
+    
+'''
